dafa9a54ac3d935c2558ad71d46733f5e1c72a54
Pascal Rapicault <pascal@rapicorp.com>
2016-08-31 16:55:52 -0400

    send message to band on page load

--- Demos/Microsoft.Band.Portable.Sample/Microsoft.Band.Portable.Sample/ClientPage.xaml.cs
+++ Demos/Microsoft.Band.Portable.Sample/Microsoft.Band.Portable.Sample/ClientPage.xaml.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 
 using Microsoft.Band.Portable.Sample.ViewModels;
+using System.Threading.Tasks;
+using Microsoft.Band.Portable.Tiles;
 
 namespace Microsoft.Band.Portable.Sample
 {
     public partial class ClientPage : BaseContentPage
     {
         private ClientViewModel clientViewModel;
+        public static BandClient band;
+        public static BandTile tile;
 
         public ClientPage(BandDeviceInfo info)
         {
@@ -35,6 +39,28 @@ namespace Microsoft.Band.Portable.Sample
         private void PersonalizationButtonClick(object sender, EventArgs e)
         {
             Navigation.PushAsync(new PersonalizationPage(clientViewModel.BandInfo, clientViewModel.BandClient));
+        }
+
+        private async void pascalButtonClick(object sender, EventArgs e)
+        {
+            await setupBand();
+            await Navigation.PushAsync(new Page1());
+        }
+
+        private async Task setupBand()
+        {
+            band = clientViewModel.BandClient;
+            var newTiles = (await band.TileManager.GetTilesAsync());
+            foreach (var item in newTiles)
+            {
+                if (item.Name == "New Tile")
+                {
+                    tile = item;
+                }
+            }
+            //Debug.WriteLine("loading tiles");
+            //tile = newTiles.Where(n => newTiles.All(o => o.Name == "New Tile")).First();
+            //Debug.WriteLine("found tile");
         }
     }
 }
---
+++ Demos/Microsoft.Band.Portable.Sample/Microsoft.Band.Portable.Sample/Page1.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Microsoft.Band.Portable.Sample
+{
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            ClientPage.band.NotificationManager.SendMessageAsync(ClientPage.tile.Id, "Hi", "My message", DateTime.Now, true);
+            //ClientPage.band.TileManager.TileButtonPressed += (sender, e) =>
+            //{
+
+            //    Debug.WriteLine("here we should go to the next page");
+            //};
+
+            //ClientPage.band.TileManager.StartEventListenersAsync();
+        }
+    }
+}
